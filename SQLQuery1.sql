dd1d5e7bc886b7983749c902c526eecf0ba5d5c5
Ankit <ankitsinghab2252@gmail.com>
2025-07-11 23:20:33 +0530

    Update all file

--- SQLQuery1.sql
+++
@@ -1,9 +0,0 @@
-select epost,count(*) as TotalEmployee from tbEmployee group by epost
-
-select epost,Sum(esalary) as TotalSalary from tbEmployee group by epost
-
-select epost,count(*) TotalEmployee ,Sum(esalary) as TotalSalary from tbEmployee group by epost
-
-
-
-
